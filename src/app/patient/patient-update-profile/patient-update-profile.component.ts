85974f0895c0937b340ad4bd0320502be3e5ab5e
Arun Mopare <arunmopare546@gmail.com>
2022-09-08 18:20:10 +0530

    added update form code

--- src/app/patient/patient-update-profile/patient-update-profile.component.ts
+++ src/app/patient/patient-update-profile/patient-update-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { SessionService } from "src/app/services/session.service";
+import { PatientAccount } from "../patient.interface";
 
 @Component({
   selector: "app-patient-update-profile",
@@ -6,8 +8,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./patient-update-profile.component.css"],
 })
 export class PatientUpdateProfileComponent implements OnInit {
+  patient: PatientAccount = {}
   u: number = 2;
-  constructor() {}
+  constructor(private _sessionService: SessionService) {
 
-  ngOnInit(): void {}
+  }
+
+  ngOnInit(): void {
+    if (this._sessionService.isPatientLoggedIn()) {
+      var session = this._sessionService.getPatientSession();
+      if (session) {
+        this.patient = JSON.parse(session)
+      }
+    }
+  }
+  onClickSubmit(result: any) {
+    console.log('====================================');
+    console.log(result);
+    console.log('====================================');
+  }
 }
